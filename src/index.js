c54d0f71343869ae6d430b9644a505bd51554764
mjmccull0 <mjmccull@uncg.edu>
2020-05-19 14:39:01 -0400

    Replaced App with the name of the app and removed some boilerplate code generated by create-react-app.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ToDo from './ToDo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToDo />
   </React.StrictMode>,
   document.getElementById('root')
 );
