c83f508952e0665c38181aea0110c4335591be14
mjmccull0 <mjmccull@uncg.edu>
2020-05-19 16:41:23 -0400

    Added state management to TodoItem.  Added controlled name and checkbox form inputs.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Checkbox from './Checkbox';
+import styles from './TodoItem.module.css';
 
 const TodoItem = (props) => {
+  const [name, setName] = useState(props.name);
+  const [complete, setComplete] = useState(props.complete);
+
+  const toggleComplete = () => {
+    setComplete(!complete);
+  }
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  }
+
   return (
     <>
       <div className="todo-item" key={props.id}>
-        {props.name}
+        <Checkbox
+          toggleCheckbox={toggleComplete}
+          checked={complete}
+        />
+        <input type="text"
+          value={name}
+          onChange={handleChangeName}
+        />
       </div>
     </>
   );
