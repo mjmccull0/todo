4abe707a97d4ed79795351951421c52b4bf11611
mjmccull0 <mjmccull@uncg.edu>
2020-05-24 15:00:53 -0400

    Adding basic UI controls to interact with lists.

--- src/Lists.js
+++ src/Lists.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Checkbox from './Checkbox';
 import styles from './Lists.module.css';
 
 const Lists = (props = {lists: []}) => {
+
   if (props.lists.length === 0) {
     return null;
+  }
+
+  const toggleComplete = () => {
+    // Toggle todo list as done.
   }
 
   return (
@@ -11,7 +17,13 @@ const Lists = (props = {lists: []}) => {
       {props.lists.map(list => (
         <li key={list.id}>
           <div className={styles.list}>
-            {list.name}
+            <Checkbox
+              toggleCheckbox={toggleComplete}
+              checked={list.complete}
+            />
+            <div className={styles.name_wrapper}>
+              {list.name}
+            </div>
           </div>
         </li>
       ))}
