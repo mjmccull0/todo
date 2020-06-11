4a00bf04016627fb0a073f41c333963538a81f98
mjmccull0 <mjmccull@uncg.edu>
2020-06-10 20:26:37 -0400

    Added task counts to tasks due today and scheduled tasks.

--- src/OverviewViewSelector.js
+++ src/OverviewViewSelector.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodoContext } from 'TodoContext';
 import Button from 'common/Button';
+import { today } from 'util/date';
 
-const OverviewViewSelector = ({click}) => (
-  <>
-    <Button label="List" onClick={click} value="0" />
-    <Button label="Today" onClick={click} value="1" />
-    <Button label="Scheduled" onClick={click} value="2" />
-  </>
-)
+const OverviewViewSelector = ({click, items}) => {
+  const {state} = useContext(TodoContext);
+  const dueToday = state.items.filter(item => item.dueDate === today());
+  const scheduledTasks = state.items.filter(item => item.dueDate !== '');
+  return (
+    <>
+      <Button label="List" onClick={click} value="0" />
+      <Button label={`Today (${dueToday.length})`} onClick={click} value="1" />
+      <Button label={`Scheduled (${scheduledTasks.length})`} onClick={click} value="2" />
+    </>
+  );
+}
 
 export default OverviewViewSelector;
