54f291674594097a5944713b9504ed70f0f6f548
mjmccull0 <mjmccull@uncg.edu>
2020-06-10 21:18:37 -0400

    Added use of context to get a count of tasks due today and task that are scheduled.

--- src/OverviewViewSelector.js
+++ src/OverviewViewSelector.js
@@ -5,13 +5,28 @@ import { today } from 'util/date';
 
 const OverviewViewSelector = ({click, items}) => {
   const {state} = useContext(TodoContext);
-  const dueToday = state.items.filter(item => item.dueDate === today());
-  const scheduledTasks = state.items.filter(item => item.dueDate !== '');
+
+  const countDueToday = () => {
+    const dueToday = state.items.filter(item => item.dueDate === today());
+    if (dueToday.length > 0) {
+      return `(${dueToday.length})`;
+    }
+    return ``;
+  }
+
+  const countScheduled = () => {
+    const scheduledTasks = state.items.filter(item => item.dueDate !== '');
+    if (scheduledTasks.length > 0) {
+      return `(${scheduledTasks.length})`;
+    }
+    return ``;
+  }
+
   return (
     <>
       <Button label="List" onClick={click} value="0" />
-      <Button label={`Today (${dueToday.length})`} onClick={click} value="1" />
-      <Button label={`Scheduled (${scheduledTasks.length})`} onClick={click} value="2" />
+      <Button label={`Today ${countDueToday()}`} onClick={click} value="1" />
+      <Button label={`Scheduled ${countScheduled()}`} onClick={click} value="2" />
     </>
   );
 }
