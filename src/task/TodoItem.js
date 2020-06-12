b967ce56fdd1e355ec2e51d9225ebd0a541fda20
mjmccull0 <mjmccull@uncg.edu>
2020-06-12 16:39:04 -0400

    Started breaking TodoItem into smaller components to allow for it to be used in multiple ways.  When viewing items of a list, items are draggable and should show the due date on the right side.  When they are shown in an overview mode, they are not draggable and should show the name of its list to the right.

--- src/task/TodoItem.js
+++ src/task/TodoItem.js
@@ -8,8 +8,9 @@ import styles from './TodoItem.module.css';
 import './TodoItem.css';
 
 import { TodoContext } from 'TodoContext';
+import Collapsible from 'common/Collapsible';
 
-const TodoItem = ({item}) => {
+const TodoItem = ({item, listName, dueDate, draggable}) => {
   const {dispatch} = useContext(TodoContext);
   const nodeRef = useRef(null);
   const [expanded, setExpanded] = useState(false);
@@ -42,57 +43,113 @@ const TodoItem = ({item}) => {
 
   return (
     <>
-      <div className={expanded ? 'open' : 'close'}>
-        <div className={`
-            ${styles.todoItem}
-            ${styles[item.priority]}
-            ${item.complete ? styles.complete : ''}
-          `}
-           key={item.id}
+      <div className={`
+          ${styles.todoItem}
+          ${styles[item.priority]}
+          ${item.complete ? styles.complete : ''}
+        `}
+         key={item.id} 
+      >
+
+        <Collapsible
+          header={
+            <>
+              <Todo
+                item={item}
+                expanded={expanded}
+                toggleComplete={toggleComplete}
+                handleUpdate={handleUpdate}
+                toggleDetails={toggleDetails}
+                className={`
+                  ${draggable ? styles.draggable : ''}
+                `}
+              />
+              {dueDate &&
+                <>
+                  <Due item={item} />
+                </>
+              }
+              {listName &&
+                  <>
+                    <ListName item={item} /> 
+                  </>
+              }
+            </>
+          }
+          section={
+            <>
+              <Details nodeRef={nodeRef}>
+                <Notes
+                  item={item}
+                  handleUpdate={handleUpdate}
+                />
+                <DueDate
+                  value={item.dueDate}
+                  onChange={handleUpdate('dueDate')}
+                />
+                <Priority
+                  priority={item.priority}
+                  onChange={handleUpdate('priority')}
+                />
+                <Delete remove={remove} />
+              </Details>
+            </>
+          }
         >
-          <div className={styles.name}>
-            <Button
-              label="&#9776;"
-              className="dragIcon"
-              onClick={toggleDetails}
-            />
-            <Checkbox
-              toggleCheckbox={toggleComplete}
-              checked={item.complete}
-            />
-            <div className={styles.name_input_wrapper}>
-              <input type="text"
-                value={item.name}
-                onChange={handleUpdate('name')}
-              />
-            </div>
-            <ArrowButton
-              direction={expanded ? 'up' : 'down'}
-              onClick={toggleDetails}
-            />
-          </div>
-          <div className={styles.details} ref={nodeRef}>
-            <div className={styles.notes}>
-              <label>
-                Notes
-              </label>
-              <textarea value={item.notes} onChange={handleUpdate('notes')} />
-            </div>
-            <div>
-              <DueDate value={item.dueDate} onChange={handleUpdate('dueDate')} />
-              <Priority
-                priority={item.priority}
-                onChange={handleUpdate('priority')}
-              />
-              <div className={styles.todo_actions}>
-                <Button className="warn" label="Delete" onClick={remove} />
-              </div>
-            </div>
-          </div>
-        </div>
+        </Collapsible> 
       </div>
     </>
   );
 }
+
+const Due = ({item}) => (
+  <div className={styles.due}>
+    {item.dueDate}
+  </div>
+)
+
+const ListName = ({item}) => {
+  const {state} = useContext(TodoContext);
+  const list = state.lists.find(list => list.id === item.listId);
+  return (
+    <div className={styles.list_name}>
+      {list.name}
+    </div>
+  );
+}
+
+const Notes = ({item, handleUpdate}) => (
+  <div className={styles.notes}>
+    <label>
+      Notes
+    </label>
+    <textarea value={item.notes} onChange={handleUpdate('notes')} />
+  </div>
+)
+const Details = ({children, nodeRef}) => (
+  <div className={styles.details} ref={nodeRef}>
+    {children}
+  </div>
+)
+
+const Delete = ({remove}) => (
+  <div className={styles.todo_actions}>
+    <Button className="warn" label="Delete" onClick={remove} />
+  </div>
+)
+const Todo = ({item, expanded, toggleComplete, handleUpdate, toggleDetails, className}) => (
+  <div className={`${styles.name} ${className}`}>
+    <Checkbox
+      toggleCheckbox={toggleComplete}
+      checked={item.complete}
+    />
+    <div className={styles.name_input_wrapper}>
+      <input type="text"
+        value={item.name}
+        onChange={handleUpdate('name')}
+      />
+    </div>
+  </div>
+)
 
 export default TodoItem;
