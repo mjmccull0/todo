e1ee61f00e774fc44441d9651a952bf30484976e
mjmccull0 <mjmccull@uncg.edu>
2020-06-18 20:23:16 -0400

    Refactored to embrace the current requirements and disregard possible future requrements (YAGNI).

--- src/list/ListOverviewMenu.js
+++ src/list/ListOverviewMenu.js
@@ -7,25 +7,35 @@ import Button from 'common/Button';
 import GridButton from 'common/GridButton';
 import ListButton from 'common/ListButton';
 
-const ListOverviewMenu = (props) => (
-  <MenuBar
-    left={
-      <CreateList />
+const ListOverviewMenu = (props) => {
+  const displaySelector = () => {
+    switch (props.display) {
+      case 'list':
+        return <GridButton onClick={() => props.onDisplayChange('grid')} />;
+      case 'grid':
+        return <ListButton onClick={() => props.onDisplayChange('list')} />;
+      default:
+        return <GridButton onClick={() => props.onDisplayChange('grid')} />;
     }
-    center={
-      <OverviewViewSelector click={props.onSelectView} />
-    }
-    right={
-      <>
-        {props.grid
-          ? <ListButton onClick={props.listMode} />
-          : <GridButton onClick={props.gridMode} />
-        }
-        <Button onClick={props.enterListSelectMode} className="checkIcon" />
-        <SearchButton onClick={props.onSearch} />
-      </>
-    }
-  />
-)
+  }
+
+  return (
+    <MenuBar
+      left={
+        <CreateList />
+      }
+      center={
+        <OverviewViewSelector click={props.onSelectView} active={props.active} />
+      }
+      right={
+        <>
+          {displaySelector()}
+          <Button onClick={props.enterListSelectMode} className="checkIcon" />
+          <SearchButton onClick={props.toggleSearch} active={props.search} />
+        </>
+      }
+    />
+  );
+}
 
 export default ListOverviewMenu;
